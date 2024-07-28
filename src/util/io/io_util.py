5c6507e9c5a43182e4d8cd69cd385c9c59dc5bcf
Chih Chien Hsieh <twcch1218@gmail.com>
2024-07-28 21:51:17 +0800

    update

--- src/util/io/io_util.py
+++ src/util/io/io_util.py
@@ -1,3 +1,7 @@
+from PIL import Image
+import io
+
+
 class IOUtil():
 
     def __init__(self):
@@ -12,3 +16,17 @@ class IOUtil():
                     data[key] = value
 
         return data
+
+    def get_image_binary(self, file_path):
+        with Image.open(file_path) as img:
+            img_byte_arr = io.BytesIO()
+            img.save(img_byte_arr, format=img.format)
+            img_byte_arr = img_byte_arr.getvalue()
+
+        return img_byte_arr
+
+    def get_image_from_binary(self, image_binary):
+        image_stream = io.BytesIO(image_binary)
+        image = Image.open(image_stream)
+
+        return image
--- src/util/mysql/create_mysql_object.py
+++ src/util/mysql/create_mysql_object.py
@@ -1,30 +1,21 @@
-import pandas as pd
 from sqlalchemy import create_engine
 
 
-class DataframeAndPostgres():
+class DataframeAndMysql():
 
     def __init__(self,
-                 database_name="postgres_data",
+                 database_name="mysql_data",
                  username="dba",
                  password="123456",
                  hostname="localhost",
-                 port="5432"):
+                 port="3306"):
         self.__database_name = database_name
         self.__username = username
         self.__password = password
         self.__hostname = hostname
         self.__port = port
-        self.__database_url = f"postgresql://{self.__username}:{self.__password}@{self.__hostname}:{self.__port}/{self.__database_name}"
-
+        self.__database_url = f"mysql+pymysql://{self.__username}:{self.__password}@{self.__hostname}:{self.__port}/{self.__database_name}"
         self.__engine = create_engine(self.__database_url)
-
-    def dataframe_to_postgres(self, table_name, dataframe, if_exists="replace", index=False):
-        dataframe.to_sql(table_name, self.__engine, if_exists=if_exists, index=index)
-
-    def dataframe_from_postgres(self, table_name):
-        dataframe = pd.read_sql(table_name, self.__engine)
-        return dataframe
 
     def get_database_name(self):
         return self.__database_name
